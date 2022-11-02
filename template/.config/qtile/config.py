b68701e6ef2ac3c440f18a5ab595b7eb8e3327a0
TheTerrior <werbird10@gmail.com>
2022-11-02 05:27:18 -0700

    Updated all changes, mostly Qtile

--- template/.config/qtile/config.py
+++ template/.config/qtile/config.py
@@ -33,19 +33,47 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen, KeyChord
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 from libqtile.command import lazy
+from xcffib.xproto import EventMask
+
+from pynput.mouse import Button, Controller
 
 home = os.path.expanduser('~')
+mouse_pynput = Controller()
+mouse_positions = []
 
+# Run at Qtile start
 @hook.subscribe.startup_once
 def autostart():
-    subprocess.run([home + "/.autorun"], shell=True)
+    global mouse_positions
+    subprocess.run([home + "/autorun.sh"], shell=True)
     
+  
+# Focus newly spawned windows, not working currently 
+@hook.subscribe.client_managed
+def client_new(client):
+    client.cmd_focus()
+    hook.fire("focus_change") 
+    
+
+# MARKED FOR UPDATING, refactor this code
+# On a new client, spawn on the same screen as the mouse initially, otherwise follow the rules 
 @hook.subscribe.client_new
 def client_new(client: backend.base.Window):
     #backend.x11.window.Window
     #backend.base.Window
     #with open(home + "/temp.txt", 'w') as file:
     #    file.write(client.name + str(client.get_wm_role()) + str(client.get_wm_type()))
+    #with open(home + "/temp.txt", 'w') as file:
+    #    file.write(qtile.core.get)
+
+    # have newly opening windows respect the mouse position
+    mouse_xpos = qtile.core.get_mouse_position()[0]
+    if mouse_xpos <= 1920:  #NOTE: this must be changed for every monitor
+        client.cmd_toscreen(0)
+    else:
+        client.cmd_toscreen(1)
+
+    # move windows to their correct workspace
     classes = client.get_wm_class()
     if 'code-oss' in classes:
         client.togroup('1')
@@ -56,13 +84,116 @@ def client_new(client: backend.base.Window):
     
     client.cmd_focus()
     hook.fire("focus_change")
+
+
+# MARKED FOR DEPRECATION
+def window_to_previous_screen(qtile, switch_group=False, switch_screen=False):
+    i = qtile.screens.index(qtile.current_screen)
+    if i != 0:
+        group = qtile.screens[i - 1].group.name
+        qtile.current_window.togroup(group, switch_group=switch_group)
+        if switch_screen == True:
+            qtile.cmd_to_screen(i - 1)
+            mouse_to_next_screen(qtile)
+    else:
+        window_to_next_screen(qtile, switch_group, switch_screen)
+
+
+# MARKED FOR UPDATING: absorb above function and also implement wrapping
+def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
+    i = qtile.screens.index(qtile.current_screen)
+    if len(qtile.screens) < 2:
+        return
+    if i + 1 != len(qtile.screens):
+        group = qtile.screens[i + 1].group.name
+        qtile.current_window.togroup(group, switch_group=switch_group)
+        if switch_screen == True:
+            qtile.cmd_to_screen(i + 1) #will need to remove the cmd_ in the future
+            mouse_to_next_screen(qtile)
+    else:
+        window_to_previous_screen(qtile, switch_group, switch_screen)
+        
+
+# Focus the next screen and bring mouse with it 
+def mouse_to_next_screen(qtile, move_focus = False):
+    global mouse_positions, mouse_pynput
     
-    with open(home + "/temp.txt", 'w') as file:
-        file.write("shit")
+    if len(mouse_positions) == 0: #initialize mouse_positions if necessary
+        initialize_mouse_positions(qtile)
+        
+    mouse_pos = qtile.core.get_mouse_position()
+    screen_index = determine_monitor(qtile, mouse_pos) #determine the current monitor
     
-    hook.fire("focus_change")
+    mouse_positions[screen_index] = mouse_pos #save the current mouse position before switching
+    
+    #switch focus and mouse position
+    mouse_pynput.position = mouse_positions[(screen_index + 1) % len(qtile.screens)]
+    if move_focus == True:
+        qtile.cmd_next_screen() #will need to remove the cmd_ in the future
+
+
+# Initialize mouse positions    
+def initialize_mouse_positions(qtile):
+    global mouse_positions
+    mouse_positions = [0]*len(qtile.screens) #set the right size
+    screen_offset = 0   #consider the size of each screen
+    for i, screen in enumerate(qtile.screens): #iterate through all screens
+        mouse_positions[i] = (screen.width / 2 + screen_offset, screen.height / 2) #set the initial place to the center of each monitor
+        screen_offset += screen.width
+    
+
+# Simple way to debug a message by writing it into ~/debug_qtile.txt
+def debug_write(message):
+    with open(home + "/debug_qtile.txt", "w") as file:
+        file.write(str(message))
+       
+
+# Open the popup calendar 
+def open_calendar(qtile):
+    subprocess.run([home + "/.config/qtile/popup-calendar.sh --popup"], shell=True)
+   
+
+# Given a set of coordinates, determine which monitor we're on 
+def determine_monitor(qtile, coords):
+    screen_index = 0
+    screen_offset = 0 #considers each monitor's width
+
+    for screen in qtile.screens:
+        if coords[0] > screen.width + screen_offset:
+            screen_index += 1
+            screen_offset += screen.width
+        else:
+            break
+        
+    return screen_index
     
     
+        
+#def mouse_move(qtile):
+#    qtile.core._root.set_attribute(eventmask=(EventMask.StructureNotify
+#                                            | EventMask.SubstructureNotify
+#                                            | EventMask.SubstructureRedirect
+#                                            | EventMask.EnterWindow
+#                                            | EventMask.LeaveWindow
+#                                            | EventMask.ButtonPress
+#                                            | EventMask.PointerMotion))
+#    def screen_change(event):
+#        assert qtile is not None
+#        if qtile.config.follow_mouse_focus and not qtile.config.cursor_warp:
+#            if hasattr(event, "root_x") and hasattr(event, "root_y"):
+#                screen = qtile.find_screen(event.root_x, event.root_y)
+#                if screen:
+#                    index_under_mouse = screen.index
+#                    if index_under_mouse != qtile.current_screen.index:
+#                        qtile.focus_screen(index_under_mouse, warp=False)
+#        qtile.process_button_motion(event.event_x, event.event_y)
+#    setattr(qtile.core, "handle_MotionNotify", screen_change)
+#
+#@hook.subscribe.startup_once
+#def my_func_name():
+#    mouse_move(qtile)
+
+
 #@hook.subscribe.startup_once
 #def autostart():
 #    processes = [
@@ -88,6 +219,10 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    #Key([mod], "i", lazy.next_screen(), desc='Focus next monitor'),
+    Key([mod], "i", lazy.function(mouse_to_next_screen, move_focus = True), desc="Focus next monitor"),
+    Key([mod, "shift"], "i", lazy.function(window_to_next_screen, switch_screen = True), desc="Follow window to next monitor"),
+    Key([mod, "control"], "i", lazy.function(window_to_next_screen), desc="Move window to next monitor"),
     #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     
     # Move windows between left/right columns or move up/down in current stack.
@@ -96,6 +231,7 @@ keys = [
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod], "s", lazy.window.toggle_floating(), desc="Toggle floating"),
     
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
@@ -141,6 +277,8 @@ keys = [
     Key([mod, "control"], "KP_Up", lazy.spawn("redshift -P -O 3500"), desc="Redshift 1"),
     Key([mod, "control"], "KP_Left", lazy.spawn("redshift -P -O 2500"), desc="Redshift 2"),
     Key([mod, "control"], "KP_Right", lazy.spawn("redshift -P -O 1500"), desc="Redshift 3"),
+    Key([mod], "Next", lazy.spawn("redshift -P -O 6500"), desc="Redshift 0"),
+    Key([mod], "Prior", lazy.spawn("redshift -P -O 3500"), desc="Redshift 1"),
 
     Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -M set Master 5%+"), desc="Volume up"),
     Key(["shift"], "XF86AudioRaiseVolume", lazy.spawn("amixer -M set Master 1%+"), desc="Volume slight up"),
@@ -179,20 +317,31 @@ for i in groups:
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
-                [mod, "control"],
+                [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # mod1 + shift + letter of group = move focused window to group
-            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            Key([mod, "control"], i.name, lazy.window.togroup(i.name),
                 desc="Move focused window to group {}".format(i.name)),
         ]
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4, insert_position=1),
+    layout.Columns(
+        #border_focus_stack=["#d75f5f", "#8f3d3d"],
+        border_focused = "#881111",
+        border_normal = "#220000",
+        border_width = 2,
+        border_on_single = True,
+        
+        insert_position = 1, #insert after the current window
+        wrap_focus_columns = False,
+        wrap_focus_rows = False,
+        wrap_focus_stacks = True,
+        ), 
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -214,31 +363,186 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
+defaults = {
+    'font': "mono",
+    'fontsize': 14,
+}
+
 screens = [
     Screen(
         bottom=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
+                #widget.CurrentScreen(
+                #    font = "mono",
+                #    fontsize = 15,
+                #    
+                #    active_text = "A",
+                #    active_color = "#00FF00",
+                #    inactive_text = "I",
+                #    inactive_color = "#FF0000",
+                #    #active_text = " ● ACTIVE ●", #"●",
+                #    #active_color = "#FF5555",
+                #    #inactive_text = " ○ ACTIVE ○", #"○",
+                #    #inactive_color = "#767676",
+                #),
+                widget.GroupBox(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                    
+                    highlight_method = "line",
+                    highlight_color = ["#464646FF", "#464646FF"], #gradient
+                   
+                    block_highlight_text_color = None, #font color for focused workspaces 
+                    active = "#FFFFFF",     #font color for workspaces with windows
+                    inactive = "#767676",   #font color for workspaces without windows, always affects this focused tab when other screen focused
+                    
+                    this_current_screen_border = "#FF5555",     #highlight current screen when current screen in focus
+                    other_screen_border = "#77DDDD",            #highlight other screen when current screen in focus
+                    
+                    this_screen_border = "#FF5555",             #highlight this screen when other screens in focus
+                    other_current_screen_border = "#77DDDD",    #highlight other screens when other screens in focus
+                    
+                    disable_drag = True, #disable dragging workspaces around
+                    borderwidth = 4, #border of weird things, but affects tag width and highlight height
+                ),
                 widget.Prompt(),
-                widget.WindowName(),
+                #widget.WindowName(),
+                widget.Spacer(),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
+                widget.CPU(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                ),
+                widget.Sep(
+                    linewidth = 0,
+                    padding = 6,
+                ),
+                widget.GenPollText(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                    update_interval=1,
+                    func=lambda: subprocess.getoutput("ip addr | grep inet | grep enp37s0 | awk '{print $2}'")
+                ),
+                widget.Sep(
+                    linewidth = 0,
+                    padding = 6,
+                ),
+                widget.CurrentLayout(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                ),
+                widget.Sep(
+                    linewidth = 0,
+                    padding = 6,
+                ),
                 #widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                #widget.Net(interface="enp37s0"),
+                #widget.GenPollText(update_interval=1, func=lambda: subprocess.check_output("sh /path/to/foo.sh").decode("utf-8")),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Sep(
+                    linewidth = 0,
+                    padding = 6,
+                ),
+                widget.KeyboardLayout(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                    configured_keyboards = ['us', 'es'],
+                    display_map = {
+                        'us': 'us',
+                        'es': 'es',
+                    }
+                ),
+                widget.Sep(
+                    linewidth = 0,
+                    padding = 6,
+                ),
+                widget.Clock(
+                    font = defaults['font'],
+                    fontsize = defaults['fontsize'],
+                    format="%d/%m/%Y %a %I:%M %p",
+                    mouse_callbacks = {
+                        'Button1': lazy.function(open_calendar),
+                    }
+                ),
+                #widget.QuickExit(),
             ],
-            24,
+            25,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            background = "#00000000",
         ),
     ),
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.GroupBox(
+                    fontsize = 15,
+                    
+                    highlight_method = "line",
+                    highlight_color = ["#464646FF", "#464646FF"], #gradient
+                   
+                    block_highlight_text_color = None, #font color for focused workspaces 
+                    active = "#FFFFFF",     #font color for workspaces with windows
+                    inactive = "#767676",   #font color for workspaces without windows, always affects this focused tab when other screen focused
+                    
+                    this_current_screen_border = "#77DDDD",     #highlight current screen when current screen in focus
+                    other_screen_border = "#FF5555",            #highlight other screen when current screen in focus
+                    
+                    this_screen_border = "#77DDDD",             #highlight this screen when other screens in focus
+                    other_current_screen_border = "#FF5555",    #highlight other screens when other screens in focus
+                    
+                    disable_drag = True, #disable dragging workspaces around
+                    borderwidth = 4, #border of weird things, but affects tag width and highlight height
+                ),
+                widget.Prompt(),
+                #widget.WindowName(),
+                widget.Spacer(),
+                widget.CurrentLayout(),
+                #widget.TextBox("default config", name="default"),
+                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.Clock(
+                    format="%d/%m/%Y %a %I:%M %p",
+                    mouse_callbacks = {
+                        'Button1': lazy.function(open_calendar),
+                    }
+                ),
+                #widget.QuickExit(),
+            ],
+            25,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            background = "#00000000",
+        ),
+    ),
+    #Screen(
+    #    bottom=bar.Bar(
+    #        [
+    #            widget.CurrentLayout(),
+    #            widget.CurrentScreen(font="mono"),
+    #            widget.GroupBox(),
+    #            widget.Prompt(),
+    #            widget.WindowName(),
+    #            #widget.Chord(
+    #            #    chords_colors={
+    #            #        "launch": ("#ff0000", "#ffffff"),
+    #            #    },
+    #            #    name_transform=lambda name: name.upper(),
+    #            #),
+    #            #widget.TextBox("default config", name="default"),
+    #            #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+    #            widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+    #        ],
+    #        25,
+    #        # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+    #        # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+    #    ),
+    #),
 ]
 
 # Drag floating layouts.
@@ -263,7 +567,11 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ]
+        Match(title="Tor Browser"),  # Needs a fixed window size to avoid fingerprinting by screen size.
+    ],
+    border_focus = "#881111",
+    border_default = "#220000",
+    border_width = 1,
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
