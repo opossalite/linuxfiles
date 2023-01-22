a8bd10e535b06707d5d74d56d7a0f221830e349e
Terrior <36648271+TheTerrior@users.noreply.github.com>
2023-01-22 06:20:36 -0800

    Update autorun.sh

--- desktop/autorun.sh
+++ desktop/autorun.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
+#devices
+xrandr --output DisplayPort-2 --primary --mode 1920x1080 --rate 165 --output HDMI-A-0 --right-of DisplayPort-2 --mode 1280x1024 --rate 75 &
 xinput set-prop "SINOWEALTH Wired Gaming Mouse" "libinput Accel Profile Enabled" 0, 1 &
 xinput set-prop "SINOWEALTH Wired Gaming Mouse" "libinput Accel Speed" 0 &
-easyeffects --gapplication-service &
+
+#background
+picom -b &
 nitrogen --restore &
-copyq &
-pasystray -g &
+easyeffects --gapplication-service &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+#system tray
 nm-applet &
+pasystray -g -m 100 --notify=none --notify=systray_action &
+copyq &
 dunst &
 flameshot &
 
