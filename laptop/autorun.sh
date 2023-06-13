4ac553a5c849696253f0383c2650608cb8a0d1c3
TheTerrior <werbird10@gmail.com>
2023-06-12 20:11:16 -0700

    Updating from laptop before moving Arch Linux -> NixOS

--- laptop/autorun.sh
+++ laptop/autorun.sh
@@ -3,6 +3,8 @@ xinput set-prop "MSFT0001:01 04F3:3140 Touchpad" "libinput Natural Scrolling Ena
 xinput set-prop "MSFT0001:01 04F3:3140 Touchpad" "libinput Accel Profile Enabled" 0, 1 &
 xinput set-prop "MSFT0001:01 04F3:3140 Touchpad" "libinput Scrolling Pixel Distance" 50 &
 xinput set-prop "MSFT0001:01 04F3:3140 Touchpad" "libinput Accel Speed" 0.5 &
+xinput set-prop "Logitech G203 LIGHTSYNC Gaming Mouse" "libinput Accel Profile Enabled" 0, 1 &
+xinput set-prop "Logitech G203 LIGHTSYNC Gaming Mouse" "libinput Accel Speed" 0 &
 
 picom --experimental-backends -b &
 nitrogen --restore &
@@ -12,4 +14,12 @@ copyq &
 flameshot &
 cbatticon -n &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
+easyeffects --gapplication-service &
+
+xset r rate 330 25 &
+
+#pipewire &
+#pipewire-pulse &
+#wireplumber &
 
